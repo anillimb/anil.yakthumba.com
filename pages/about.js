78aed70ccc4f87c18955f690a7746cc2d5dd1f67
Anil Limbu <limb123456789@gmail.com>
2021-07-24 11:02:28 +0545

    navbar added

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+function About() {
+  return(
+		<div>
+			<h1>About page</h1>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
 
@@ -11,9 +12,12 @@ const Home = () => {
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 			<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"></link>
+			{/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+			<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet"></link> */}
 		</Head>
 		<main className={styles.main}>
-			<h1>This is next.js project</h1>
+			<Navbar />
 		</main>
 		</div>
 	);
